484171df373172dc2351cfafddb9c5e2af0a5894
jamesbirrellgray <james@jamesbirrellgray.com>
2018-11-19 17:06:33 +1100

    refactor: remove db.json from the repo

--- back-end/index.js
+++ back-end/index.js
@@ -2,13 +2,23 @@ const express = require('express'),
       jsonfile = require('jsonfile'),
       path = require('path'),
       bodyParser = require('body-parser'),
+      fileExists = require('file-exists'),
       db = path.resolve(__dirname, 'db/db.json'),
+      seedData = path.resolve(__dirname, 'seed_data/cmsData.json'),
       app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.json());
 
+// Get seed command argument
+var argv = require('minimist')(process.argv.slice(2));
+console.log(argv._);
+// Set up the DB
+
+if(argv._.length === 0 && fileExists.sync(db) === false) {
+  jsonfile.writeFileSync(db, jsonfile.readFileSync(seedData));
+}
 // Set port
 var port = process.env.PORT || 5000;
 
