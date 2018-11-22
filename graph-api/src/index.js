bfa15ef61cb8a3fcb4480a3e919ad4229aca933c
jamesbirrellgray <james@jamesbirrellgray.com>
2018-11-22 11:35:25 +1100

    refactor: finally fixed the graph-api build and watching, it now updates the schema.graphql file after changes ... noice

---
+++ graph-api/src/index.js
@@ -0,0 +1,8 @@
+
+import app from "./app"
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.info('Running a GraphQL API server at localhost:'+PORT+'/graphql')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const StartServerPlugin = require('start-server-webpack-plugin');
 
-module.exports = [{
+module.exports = {
   name: "client",
   entry: {
     client: './front-end/src/client.js',
@@ -18,64 +18,4 @@ module.exports = [{
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
  }
-},
-// Graph-API Pack
-{
-  name: "graph",
-  mode: "development",
-  entry: ['./graph-api/src'],
-  watch: true,
-  watchOptions: {
-    poll: true
-  },
-  target: 'node',
-  node: {
-      __filename: true,
-      __dirname: true
-  },
-  externals: [nodeExternals({ whitelist: ['webpack/hot/poll?1000'] })],
-  module: {
-      rules: [
-          {
-              test: /\.js?$/,
-              use: [
-                  {
-                      loader: 'babel-loader',
-                      options: {
-                          babelrc: false,
-                          presets: [["@babel/env", { "targets": { "node": "current" } }]],
-                          plugins: ['@babel/plugin-transform-regenerator', '@babel/plugin-transform-runtime']
-                      }
-                  }
-              ],
-              exclude: /node_modules/
-          }
-      ]
-  },
-  serve: { //object
-    port: 4000,
-    content: './dist',
-    // ...
-  },
-  // lets you provide options for webpack-serve
-  stats: "errors-only",  // lets you precisely control what bundle information gets displayed
-  devServer: {
-    contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
-    compress: true, // enable gzip compression
-    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
-    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-    https: false, // true for self-signed, object for cert authority
-    noInfo: true, // only errors & warns on hot reload
-  },
-  plugins: [
-      new StartServerPlugin('server.js'),
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.DefinePlugin({
-          'process.env': { BUILD_TARGET: JSON.stringify('server') }
-      })
-  ],
-  output: { path: path.join(__dirname, 'graph-api/dist'), filename: 'server.js' }
-},
-]
+}
